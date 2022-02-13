07c081f5b00fdf063c52c11ca13f03780a545e79
Max Pöttke <max-poettke@gmx.de>
2022-02-13 18:55:54 +0100

    feat: added The Construct and pathing

---
+++ unity/PoseidonVR/Assets/Scripts/pathfinding.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pathfinding : MonoBehaviour
+{
+
+    void Start(){
+        for (int i = 0; i < transform.childCount; i ++){
+            var child = transform.GetChild(i);
+            var child2 = transform.GetChild(i >= transform.childCount - 1 ? 0 : i + 1);
+            
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.localScale = new Vector3(10f,10f,10f);
+            sphere.transform.position = child.transform.position;
+            /*if(i < transform.childCount - 1) {
+
+            } todo draw Line using linerenderer*/
+        }
+    }
+    void OnDrawGizmos()
+    {
+        for (int i = 0; i < transform.childCount; i ++){
+            var child = transform.GetChild(i);
+            var child2 = transform.GetChild(i >= transform.childCount - 1 ? 0 : i + 1);
+            Gizmos.DrawSphere(child.position, 5f);
+            if(i < transform.childCount - 1) {
+                Gizmos.DrawLine(child.position, child2.position);
+            }
+        }
+    }
+}
---
+++ unity/PoseidonVR/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playerMovement : MonoBehaviour
+{
+
+    private GameObject path;
+    public List<Transform> waypoints;
+    private int currentWaypoint = 0; 
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = GameObject.FindGameObjectWithTag("Path");
+        waypoints = new List<Transform>();
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        for (int i = 0; i < path.transform.childCount; i ++){
+            waypoints.Add(path.transform.GetChild(i));
+        }
+
+        agent.SetDestination(waypoints[currentWaypoint].transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.remainingDistance < 1f && currentWaypoint < waypoints.Count - 1){
+            Debug.Log("failed" + currentWaypoint);
+            currentWaypoint ++;
+            agent.SetDestination(waypoints[currentWaypoint].transform.position);
+        }
+    }
+}
