8cfed8eb48a7ed3ee3e2f52affd181ee39b5a7de
Max Pöttke <max-poettke@gmx.de>
2022-03-02 14:45:51 +0100

    feat: cannons now target only things in sight. Error with rotation

--- unity/PoseidonVR/Assets/TowerAI.cs
+++ unity/PoseidonVR/Assets/TowerAI.cs
@@ -7,7 +7,6 @@ public class TowerAI : MonoBehaviour
     [SerializeField] string targeting = "first";
     [SerializeField] float shotDelay;
     
-    public List<Transform> cannons;
     public List<GameObject> targets;
     public GameObject currTarget;
     
@@ -15,9 +14,6 @@ public class TowerAI : MonoBehaviour
     private bool shooting = false;
 
     void Start(){
-        foreach(Transform child in transform){
-            cannons.Add(child);
-        }
     }
 
     void OnTriggerEnter(Collider col){
@@ -40,34 +36,44 @@ public class TowerAI : MonoBehaviour
         }
     }
 
+
+    //todo: make "first" target not only first in the list but also first in the game => consider distance from next NavMesh point and index of NavMesh point
     IEnumerator shoot(string currTargeting, float delay){
         while(shooting){
             yield return new WaitForSeconds(delay);
             if(targets.Count > 0){
-                switch(currTargeting){
-                    case "first":
-                        currTarget = targets[0];
-                        break;
+
+                if(currTarget == null){
+                    currTarget = targets[0];
                 }
 
-                foreach(Transform cannon in cannons){
-                    var child = cannon.GetChild(0).gameObject.transform;
-                    angleCannon(child);
-                }  
+                Transform barrel = transform.GetChild(0).gameObject.transform;
+                targetEnemy(barrel);
+
             } else {
                 shooting = false;
             }
         }
-        
     }
 
-    void angleCannon(Transform cTransform){
-        Vector3 dir = -(targets[0].transform.position - cTransform.position).normalized;
-        Quaternion test = cTransform.rotation;
-        cTransform.rotation = Quaternion.LookRotation(dir);
-        Debug.Log(cTransform.localEulerAngles.y);
-        if(cTransform.localEulerAngles.y - 360 < -30 || cTransform.localEulerAngles.y - 360 > 30){
-            cTransform.rotation = test;
+
+    // todo make cannon target only enemies it can also hit
+    // idea: when out of range choose next one in List (potentially a loop)
+    void targetEnemy(Transform bTransform){
+
+        Quaternion temp = bTransform.rotation;
+
+        for(int i = 0; i < targets.Count; i ++){
+            Vector3 dir = -(targets[i].transform.position - bTransform.position).normalized;
+            bTransform.rotation = Quaternion.LookRotation(dir);
+            //checks angle of the barrel and sets currTarget to the first target with angle < 60°
+            //todo: fix the fact that it only checks if the angle > -30° since positive values dont seem to show up 
+            if(bTransform.localEulerAngles.y - 360 > -30 && bTransform.localEulerAngles.y - 360 < 30){
+                currTarget = targets[i];
+                break;
+            }
+            bTransform.rotation = temp;
+            
         }
     }
 }
